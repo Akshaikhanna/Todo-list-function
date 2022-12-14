b3d37725d419b5c029cd9e932a0d17634df1e615
Akshaikhanna <90137770+Akshaikhanna@users.noreply.github.com>
2022-12-14 11:31:47 +0530

    ak

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.css'
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editId, setEditId] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (editId) {
+      const editTodo = todos.find((i) => i.id === editId);
+      const updateTodos = todos.map((t) => t.id === editTodo.id
+        ? (t = { id: t.id, todo })
+        : { id: t.id, todo: t.todo }
+      );
+      setTodos(updateTodos);
+      setEditId(0);
+      setTodo("");
+      return;
+    }
+
+    if (todo !== '') {
+      setTodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos]);
+      console.log(todo);
+      setTodo("");
+    }
+  };
+
+  const handleDelete = (id) => {
+    const delTodo = todos.filter((to) => to.id !== id);
+    setTodos([...delTodo])
+  }
+
+  const handleEdit = (id) => {
+    const editTodo = todos.find((i) => i.id === id);
+    setTodo(editTodo.todo);
+    setEditId(id);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ToDo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type='text' className='box' value={todo} onChange={(e) => setTodo(e.target.value)} placeholder='Enter the message' />
+        <button className='btn' type='submit'>{ editId ? "Edit" : "ADD" }</button>
+      </form>
+      <div>
+        <ul>
+          {todos.map((t) => (
+            <li className="singleTodo">
+              <span className="todoText" key={t.id}>
+                {t.todo}
+              </span>
+              <button className='btn1' onClick={() => handleDelete(t.id)} >Delete</button>
+              <button className='btn2' onClick={() => handleEdit(t.id)} >Edit</button>
+            </li>
+          ))}
+
+        </ul>
+      </div>
     </div>
   );
 }
